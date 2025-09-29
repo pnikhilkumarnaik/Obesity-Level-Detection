c86debcf68c05af4b56a1243c1b80be8294aa81d
P Nikhil Kumar Naik <pnikhilkumarnaik@gmail.com>
2025-09-29 21:31:31 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="obesity_detection",           
+    version="0.1.0",                   
+    author=" NIKHIL NAIK",                    
+    author_email="pnikhilkumarnaik@example.com",
+    description="A machine learning project for obesity detection",
+    packages=find_packages(),           
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "imbalanced-learn",
+        "scikit-learn",
+        "lightgbm",
+        "xgboost",
+        "joblib"
+    ],
+    python_requires='>=3.8',              
+)
